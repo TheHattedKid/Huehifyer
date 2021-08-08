979854f4c42b34d2115a29034e463837fa1d67eb
hatkid <daichi93100@gmail.com>
2021-08-08 18:35:14 +0200

    GUYS WE GOT COLORS 8 MAYBE BUT ITS SOMETHING ALSO RED IS SUS

--- src/com/devhonk/huehify/Main.java
+++ src/com/devhonk/huehify/Main.java
@@ -24,7 +24,133 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+class Dither {
+	static class C3 {
+		int r, g, b;
+
+		public C3(int c) {
+			Color color = new Color(c);
+			r = color.getRed();
+			g = color.getGreen();
+			b = color.getBlue();
+		}
+
+		public C3(int r, int g, int b) {
+			this.r = r;
+			this.g = g;
+			this.b = b;
+		}
+
+		public C3 add(C3 o) {
+			return new C3(r + o.r, g + o.g, b + o.b);
+		}
+
+		public int clamp(int c) {
+			return Math.max(0, Math.min(255, c));
+		}
+
+		public int diff(C3 o) {
+			int Rdiff = o.r - r;
+			int Gdiff = o.g - g;
+			int Bdiff = o.b - b;
+			int distanceSquared = Rdiff * Rdiff + Gdiff * Gdiff + Bdiff * Bdiff;
+			return distanceSquared;
+		}
+
+		public C3 mul(double d) {
+			return new C3((int) (d * r), (int) (d * g), (int) (d * b));
+		}
+
+		public C3 sub(C3 o) {
+			return new C3(r - o.r, g - o.g, b - o.b);
+		}
+
+		public Color toColor() {
+			return new Color(clamp(r), clamp(g), clamp(b));
+		}
+
+		public int toRGB() {
+			return toColor().getRGB();
+		}
+	}
+
+	private static C3 findClosestPaletteColor(C3 c, C3[] palette) {
+		C3 closest = palette[0];
+
+		for (C3 n : palette) {
+			if (n.diff(c) < closest.diff(c)) {
+				closest = n;
+			}
+		}
+
+		return closest;
+	}
+
+	static BufferedImage floydSteinbergDithering(BufferedImage img) {
+
+		C3[] palette = new C3[]{
+				new C3(0, 0, 0), // black
+				new C3(0, 0, 255), // green
+				new C3(0, 255, 0), // blue
+				new C3(0, 255, 255), // cyan
+				new C3(255, 0, 0), // red
+				new C3(255, 0, 255), // purple
+				new C3(255, 255, 0), // yellow
+				new C3(255, 255, 255)  // white
+		};
+
+		int w = img.getWidth();
+		int h = img.getHeight();
+
+		C3[][] d = new C3[h][w];
+
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				d[y][x] = new C3(img.getRGB(x, y));
+			}
+		}
+
+		for (int y = 0; y < img.getHeight(); y++) {
+			for (int x = 0; x < img.getWidth(); x++) {
+
+				C3 oldColor = d[y][x];
+				C3 newColor = findClosestPaletteColor(oldColor, palette);
+				img.setRGB(x, y, newColor.toColor().getRGB());
+
+				C3 err = oldColor.sub(newColor);
+
+				if (x + 1 < w) {
+					d[y][x + 1] = d[y][x + 1].add(err.mul(7. / 16));
+				}
+
+				if (x - 1 >= 0 && y + 1 < h) {
+					d[y + 1][x - 1] = d[y + 1][x - 1].add(err.mul(3. / 16));
+				}
+
+				if (y + 1 < h) {
+					d[y + 1][x] = d[y + 1][x].add(err.mul(5. / 16));
+				}
+
+				if (x + 1 < w && y + 1 < h) {
+					d[y + 1][x + 1] = d[y + 1][x + 1].add(err.mul(1. / 16));
+				}
+			}
+		}
+
+		return img;
+	}
+}
 public class Main extends Application {
 
     //GET READY TO SEE MY BAD CODE
@@ -67,9 +193,10 @@ public class Main extends Application {
     }
 
     public static void conv(String in, int w, int h, String outF, String paletteFolder) throws IOException {
+        System.out.println("In: " + in);
         BufferedImage bi = ImageIO.read(new File(in));
 
-        BufferedImage scaledImg = getScaledImage(bi, w, h);
+        BufferedImage scaledImg = Dither.floydSteinbergDithering(getScaledImage(bi, w, h));
         BufferedImage out = new BufferedImage(w*60, h*40, BufferedImage.TYPE_INT_ARGB);
 
         Graphics2D g = (Graphics2D) out.getGraphics();
@@ -77,8 +204,10 @@ public class Main extends Application {
         for (int y = 0; y < scaledImg.getHeight(); y++) {
             for (int x = 0; x < scaledImg.getWidth(); x++) {
                 Color oc = new Color(scaledImg.getRGB(x, y));
-                double avg = ((oc.getRed() + oc.getBlue() + oc.getGreen()) / 3.0) / 255.0;
-                int c = (int) Math.floor(avg * (6 - 1));
+                boolean re = oc.getRed() > 127;
+                boolean gr = oc.getGreen() > 127;
+                boolean bl = oc.getBlue() > 127;
+                String c = String.format("%d%d%d", re ? 1 : 0, gr ? 1 : 0, bl ? 1 : 0);
                 g.drawImage(ImageIO.read(new File(paletteFolder + c + ".png")), x*60, y*40, null);
             }
         }
@@ -97,8 +226,13 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public static void main(String[] args) throws IOException {
+        /*for (int i = 0; i < 8; i++) {
 
-    public static void main(String[] args) {
+            String path = "/home/hatkid/Bureau/grayhueh/color/" + String.format("%3s", Integer.toBinaryString(i)).replace(" ", "0") + ".png";
+            System.out.println(path);
+            ImageIO.write(getScaledImage(ImageIO.read(new File(path)), 60, 40), "PNG", new File(path));
+        }*/
         launch(args);
 
     }
@@ -107,13 +241,13 @@ public class Main extends Application {
     public void huehify(ActionEvent actionEvent) {
         new Thread(() -> {
             Platform.runLater(() -> {
-                if (!new File(System.getProperty("user.home") + File.separatorChar + ".huehs").isDirectory()) {
+                if (!new File(System.getProperty("user.home") + File.separatorChar + ".huehs" + File.separatorChar + "colors").isDirectory()) {
                     System.out.println("Hueh folder does not exist! Creating one...");
-                    new File(System.getProperty("user.home") + File.separatorChar + ".huehs").mkdirs();
+                    new File(System.getProperty("user.home") + File.separatorChar + ".huehs" + File.separatorChar + "colors").mkdirs();
                     try {
-                        for (int i = 0; i < 6; i++) {
+                        for (int i = 0; i < 8; i++) {
                             //you may do a custom thing later on
-                            BufferedImage bi = ImageIO.read(new URL("https://devhonk.github.io/huehs/" + i + ".png"));
+                            BufferedImage bi = ImageIO.read(new URL("https://devhonk.github.io/huehs/colors/" + String.format("%3s", Integer.toBinaryString(i)).replace(" ", "0") + ".png"));
                             ImageIO.write(
                                     bi,
                                     "PNG",
@@ -122,7 +256,9 @@ public class Main extends Application {
                                                     File.separatorChar +
                                                     ".huehs" +
                                                     File.separatorChar +
-                                                    i +
+                                                    "colors" +
+                                                    File.separatorChar +
+                                                    String.format("%3s", Integer.toBinaryString(i)).replace(" ", "0") +
                                                     ".png"
                                     )
                             );
@@ -141,14 +277,20 @@ public class Main extends Application {
                                 System.getProperty("user.home") +
                                         File.separatorChar +
                                         ".huehs" +
-                                        File.separatorChar);
+                                        File.separatorChar +
+                                        "colors" +
+                                        File.separatorChar
+                                );
                     } else {
                         for (int i = 0; i < input.size(); i++) {
                             conv(input.get(i), w, h, new File(output).getParentFile().getPath() + File.separatorChar + (new File(output).getName().split("\\.[^\\.]")[0] + i) + (new File(output).getName().endsWith(".png") || new File(output).getName().endsWith(".PNG") ? "" : ".png"),
                                     System.getProperty("user.home") +
                                             File.separatorChar +
                                             ".huehs" +
-                                            File.separatorChar);
+                                            File.separatorChar +
+                                            "colors" +
+                                            File.separatorChar
+                                    );
                         }
                     }
                 } catch (IOException e) {
